12d1acead290e909dc0d95af3b907476798ddec2
Miro Jurisic <miro.jurisic@bmw.at>
2024-01-31 09:44:04 +0100

    resubmit ready

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,7 @@
 # DONE Designate a cloud provider, region, and credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "vscode"
+  profile = "miro-udacity"
 }
 
 # DONE: provision 4 AWS t2.micro EC2 instances named Udacity T2
@@ -9,6 +9,9 @@ resource "aws_instance" "Udacity_T2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   count         = 4
+  tags = {
+    Name = "Udacity T2"
+  }
 }
 
 # DONE: provision 2 m4.large EC2 instances named Udacity M4
@@ -16,6 +19,9 @@ resource "aws_instance" "Udacity_T2" {
 #   ami           = data.aws_ami.ubuntu.id
 #   instance_type = "m4.large"
 #   count         = 2
+#   tags = {
+#     Name = "Udacity M4"
+#   }
 # }
 
 
